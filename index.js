20f72c80373af617b0516050ebb796159be04cc7
samanthahuaa <65002888+samanthahuaa@users.noreply.github.com>
2020-06-25 13:39:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-import React from 'React'
-import ReactDOM from 'react-dom'
 import todolist from './todolist'
+
+<script crossorigin src="https://unpkg.com/react@16/umd/react.development.js"></script>
+<script crossorigin src="https://unpkg.com/react-dom@16/umd/react-dom.development.js"></script>
 
 ReactDOM.render(<todolist />,document.getElementById('root'))
